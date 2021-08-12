9320a5c4d3235c7ae1414308e4bf481ab49f084b
Ks Renu <renuks0802@gmail.com>
2021-08-12 10:38:33 -0700

    Codes

---
+++ list problems/multiplicationTable.py
@@ -0,0 +1,17 @@
+#4th line print indicates the 4 tab space.
+#ctrl+k+c=to comment many lines
+X = 1
+Y = 1
+print ('{:>4}'.format(' '), end= ' ')
+for X in range(1, 11):
+ print('{:>4}'.format(X), end=' ')
+ 
+print()
+ 
+for X in range(1,11):
+ print('{:>4}'.format(X), end=' ')
+ while Y <= 10:
+  print('{:>4}'.format(X * Y), end=' ')
+  Y+=1
+ print()
+ Y=1
